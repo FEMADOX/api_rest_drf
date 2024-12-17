0c59ae78681e8163f446df1686308b830250fdf2
Fenyxz <enmadofenyxz@gmail.com>
2024-12-17 16:32:34 +0000

    Login SignUp UserDetailViews

--- api_auth/views.py
+++ api_auth/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
-
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
